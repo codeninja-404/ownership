25f624d190c50a863ed026e332c84e138a347d3b
codeninja-404 <saidularefin8@gmail.com>
2026-01-01 04:33:32 +0600

    Refactor main function to demonstrate ownership transfer in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String :: from ("hello" );
+    take_ownership(&s);
+    println!("{}",s);
+
+}
+
+
+fn take_ownership(text:&String){
+    println!("{}",text);
 }
