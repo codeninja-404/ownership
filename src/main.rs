6628894af5b1bb6345287a354ccc7170c3e78132
codeninja-404 <saidularefin8@gmail.com>
2026-01-08 16:11:59 +0600

    Uncomment main function and update ownership demonstration with mutable references

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-fn main() {
-    let s = String :: from ("hello" );
-    take_ownership(&s);
-    println!("{}",s);
+// fn main() {
+//     let s = String :: from ("hello" );
+//     take_ownership(&s);
+//     println!("{}",s);
 
-}
+// }
 
 
-fn take_ownership(text:&String){
-    println!("{}",text);
+// fn take_ownership(text:&String){
+//     println!("{}",text);
+// }
+fn main (){
+    let mut x = 10;
+    
+    {
+    let a = &mut x; 
+    *a +=1;
+    }
+
+    {
+     let b = &mut x; 
+    *b +=1;
+    }
+    
+   
+
+    println!("{}",x);
 }
